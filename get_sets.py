49815e4f5cafc56236671e79030a9eb8234a4fbf
Michael Burdick <asylumrunner@gmail.com>
2023-06-20 14:16:15 -0700

    pull set images from scryfall

--- get_sets.py
+++ get_sets.py
@@ -2,7 +2,12 @@ import requests
 import json
 
 r = requests.get('https://api.scryfall.com/sets').json()
-trimmed_sets = {set["code"]: { "name": set["name"], "date": set["released_at"]} for set in r['data'] if (set['set_type'] in ['core', 'expansion'])}
+trimmed_sets = {set["code"]: { "name": set["name"], "img_url": set['icon_svg_uri'], "date": set["released_at"]} for set in r['data'] if (set['set_type'] in ['core', 'expansion'])}
 
-with open('./cast-through-time/src/data/set-data.json', 'w') as datafile:
+for set_code in trimmed_sets:
+    set_symbol = requests.get(trimmed_sets['set_code']['img_url'])
+    with open('./cast_through_time/data/card_images/{}.svg'.format(set_code), 'w+') as img_file:
+        img_file.write(set_symbol)
+
+with open('./cast-through-time/src/data/set-data.json', 'w+') as datafile:
     json.dump(trimmed_sets, datafile)
