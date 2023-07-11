d022174cd32baf4d931f871595b2d039ea71a2bf
Michael Burdick <asylumrunner@gmail.com>
2023-07-11 16:27:43 -0700

    update header and footer

--- cast-through-time/src/components/Footer.js
+++ cast-through-time/src/components/Footer.js
@@ -1,5 +1,7 @@
 function Footer() {
-    return <div>Footer</div>
+    return (<div className="flex-col justify-items-center items-center pt-8">
+        <p className="text-sm text-center">Created by Michael Burdick. Portions of Cast Through Time are unofficial Fan Content permitted under the Wizards of the Coast Fan Content Policy. The literal and graphical information presented on this site about Magic: The Gathering, including card images and mana symbols, is copyright Wizards of the Coast, LLC. Cast Through Time is not produced by or endorsed by Wizards of the Coast.</p> 
+    </div>)
 }
 
 export default Footer;
--- cast-through-time/src/components/InfoCard.js
+++ cast-through-time/src/components/InfoCard.js
@@ -2,7 +2,7 @@ import '../keyrune-master/keyrune-master/css/keyrune.css';
 
 function InfoCard({card}) {
     const iconName = `ss ss-4x ss-rare ss-grad ss-${card.set}`
-    return (<div className='border border-red-500'>
+    return (<div className='border border-red-500 m-4'>
         <div className="m-2 flex flex-row items-center justify-between">
             <div className="flex-col justify-items-center">
                 <h1 className="text-3xl font-bold underline justify-self-center">{card.header}</h1>
