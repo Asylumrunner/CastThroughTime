183ffade196a2d0d440c8d07f44677ea89b13618
Michael Burdick <asylumrunner@gmail.com>
2023-07-16 19:12:04 -0700

    add img

--- cast-through-time/src/App.js
+++ cast-through-time/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CardList from "./components/CardList";
 import SetSelectorToggle from "./components/SetSelectorToggle";
+
 function App() {
   return (<div>
     <Header />
