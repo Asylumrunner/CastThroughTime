aa7706bfc421b8cdc49678ead9682abba09c7030
Michael Burdick <asylumrunner@gmail.com>
2025-04-21 15:21:19 -0700

    clean up card formatting when no img

--- cast-through-time/src/components/InfoCard.js
+++ cast-through-time/src/components/InfoCard.js
@@ -10,13 +10,16 @@ function InfoCard({card}) {
         }
     }
 
+    const condtlImgFormatting = card.img ? "basis-1/2" : "ml-10"
+    const classNames = `${condtlImgFormatting} flex flex-col px-8 lg:pl-0 self-center`
+
     const imagePath = checkImageExistsLocal() ? `../images/${card.set}.svg` : `https://castthroughtime.s3.us-west-1.amazonaws.com/setsymbol/${card.set}.svg`
 
     return (<div className="flex flex-col lg:flex-row snap-start h-screen md:pt-20 mb-30 lg:mb-0">
         {card.img && <div className="basis-1/2 self-center justify-self-center">
             <img className="size-1/3 lg:size-auto" alt="An example for this card" src={`/img/${card.img}`}/>
         </div>}
-        <div className="basis-1/2 flex flex-col px-8 lg:pl-0 self-center">
+        <div className={classNames}>
             <div className="flex flex-row content-center">
                 <img src={imagePath} alt={card.set} title={`Introduced in ${card.set.toUpperCase()}`}className="w-10 h-10 mr-3" />
                 <h1 style={{fontFamily: 'PT Serif'}} className="text-3xl font-bold self-center">{card.header}</h1>  
